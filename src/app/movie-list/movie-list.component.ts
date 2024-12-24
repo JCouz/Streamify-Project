d4b1346c5e9c2431ccbdb2fd69aed10780b62707
JCouz <couzensj@live.co.uk>
2024-12-24 13:26:12 +0000

    Add movie list

---
+++ src/app/movie-list/movie-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-list',
+  standalone: false,
+  
+  templateUrl: './movie-list.component.html',
+  styleUrl: './movie-list.component.css'
+})
+export class MovieListComponent {
+
+}
