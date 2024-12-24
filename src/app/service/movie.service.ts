7e03ef7bc32a0b5bb300403b9285952ff3971fed
JCouz <couzensj@live.co.uk>
2024-12-24 14:09:32 +0000

    Refactor to use json for data

--- src/app/service/movie.service.ts
+++ src/app/service/movie.service.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 export interface Movie {
   title: string;
@@ -11,16 +12,10 @@ export interface Movie {
   providedIn: 'root',
 })
 export class MovieService {
-  private movies: Movie[] = [
-    { title: 'The Shawshank Redemption', genre: 'Drama', rating: 9.3 },
-    { title: 'The Godfather', genre: 'Crime', rating: 9.2 },
-    { title: 'The Dark Knight', genre: 'Action', rating: 9.0 },
-    { title: '12 Angry Men', genre: 'Drama', rating: 9.0 },
-    { title: 'Pulp Fiction', genre: 'Crime', rating: 8.9 },
-    { title: 'The Good, the Bad and the Ugly', genre: 'Western', rating: 8.8 },
-  ];
+  private moviesUrl = 'assets/movies.json';
+  constructor(private http: HttpClient) {}
 
   getMovies(): Observable<Movie[]> {
-    return of(this.movies);
+    return this.http.get<Movie[]>(this.moviesUrl);
   }
 }
