0edc62d77fbb109681109cc5499eb8486eb919c0
JCouz <couzensj@live.co.uk>
2024-12-24 14:53:47 +0000

    Add 404 component and paths to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { Error404Component } from './error-404/error-404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MovieListComponent },
+  { path: '**', component: Error404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { Error404Component } from './error-404/error-404.component';
 
 @NgModule({
-  declarations: [AppComponent, MovieListComponent],
+  declarations: [AppComponent, MovieListComponent, Error404Component],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
